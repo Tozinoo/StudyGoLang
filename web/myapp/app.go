aa2495519664eb75aeba0ee80a488cd9276d6338
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2022-05-26 19:46:21 +0900

    Test 환경

---
+++ web/myapp/app.go
@@ -0,0 +1,57 @@
+package myapp
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type User struct {
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+type fooHandler struct{}
+
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Bad Request: ", err)
+		return
+	}
+	w.Header().Add("content-type", "application/json")
+	user.CreatedAt = time.Now()
+	data, _ := json.Marshal(user)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, string(data))
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!", name)
+}
+
+func NewHttpHandler() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
+
+	mux.HandleFunc("/bar", barHandler)
+
+	mux.Handle("/foo", &fooHandler{})
+
+	http.ListenAndServe(":3000", mux)
+
+	return mux
+}
